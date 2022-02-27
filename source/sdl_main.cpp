b91e5a079b72e2dd0eece7d19c2cda8e2677607d
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-02-27 15:48:40 +0100

    Start learning SDL2.

--- source/sdl_main.cpp
+++ source/sdl_main.cpp
@@ -3,97 +3,18 @@
 //
 
 #include <SDL2/SDL.h>
-#define SCREEN_WIDTH    800
-#define SCREEN_HEIGHT   600
-// Define MAX and MIN macros
-#define MAX(X, Y) (((X) > (Y)) ? (X) : (Y))
-#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
+#include <SDL2/SDL_opengl.h>
+#include <string>
+
 
 int main()
 {
-	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-		printf("SDL could not be initialized!\n"
-			   "SDL_Error: %s\n", SDL_GetError());
-		return 0;
-	}
-
-	SDL_Window *window = SDL_CreateWindow("Basic C SDL project",
-										  SDL_WINDOWPOS_UNDEFINED,
-										  SDL_WINDOWPOS_UNDEFINED,
-										  SCREEN_WIDTH, SCREEN_HEIGHT,
-										  SDL_WINDOW_SHOWN);
-	if(!window)
-	{
-		printf("Window could not be created!\n"
-			   "SDL_Error: %s\n", SDL_GetError());
-	}
-	else
-	{
-		// Create renderer
-		SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-		if(!renderer)
-		{
-			printf("Renderer could not be created!\n"
-				   "SDL_Error: %s\n", SDL_GetError());
-		}
-		else
-		{
-			// Declare rect of square
-			SDL_Rect squareRect;
-
-			// Square dimensions: Half of the min(SCREEN_WIDTH, SCREEN_HEIGHT)
-			squareRect.w = MIN(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
-			squareRect.h = MIN(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
-
-			// Square position: In the middle of the screen
-			squareRect.x = SCREEN_WIDTH / 2 - squareRect.w / 2;
-			squareRect.y = SCREEN_HEIGHT / 2 - squareRect.h / 2;
-
-
-			// Event loop exit flag
-			bool quit = false;
-
-			// Event loop
-			while(!quit)
-			{
-				SDL_Event e;
-
-				// Wait indefinitely for the next available event
-				SDL_WaitEvent(&e);
-
-				// User requests quit
-				if(e.type == SDL_QUIT)
-				{
-					quit = true;
-				}
-
-				// Initialize renderer color white for the background
-				SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-
-				// Clear screen
-				SDL_RenderClear(renderer);
-
-				// Set renderer color red to draw the square
-				SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
-
-				// Draw filled square
-				SDL_RenderFillRect(renderer, &squareRect);
-
-				// Update screen
-				SDL_RenderPresent(renderer);
-			}
-
-			// Destroy renderer
-			SDL_DestroyRenderer(renderer);
-		}
-
-		// Destroy window
-		SDL_DestroyWindow(window);
-	}
-
-	// Quit SDL
+	SDL_Init(SDL_INIT_VIDEO);
+	auto title = "My first window";
+	SDL_Window * window = SDL_CreateWindow(title, 300,30, 500, 600, SDL_WINDOW_SHOWN);
+	SDL_Surface * screen = SDL_GetWindowSurface(window);
+	SDL_Delay(2000);
+	SDL_DestroyWindow(window);
 	SDL_Quit();
-
 	return 0;
-
 }
